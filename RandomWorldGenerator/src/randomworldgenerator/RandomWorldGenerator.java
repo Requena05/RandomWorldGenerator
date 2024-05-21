5018f9d8081e41bfebb800b0df9ec82f235eb994
unknown <franreke506@gmail.com>
2024-05-21 14:04:19 +0200

    Actualizacion

--- RandomWorldGenerator/src/randomworldgenerator/RandomWorldGenerator.java
+++ RandomWorldGenerator/src/randomworldgenerator/RandomWorldGenerator.java
@@ -16,6 +16,7 @@ public class RandomWorldGenerator {
      */
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Hola Universo");   
     }
     
 }
